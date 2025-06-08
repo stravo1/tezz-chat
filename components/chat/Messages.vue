a26d1a848d78b1b63b805346e47b4b32981293e7
stravo1@mac <stravoone@gmail.com>
2025-06-08 18:30:53 +0530

    feat(chat): enhance message rendering with syntax highlighting

--- components/chat/Messages.vue
+++ components/chat/Messages.vue
@@ -1,6 +1,19 @@
 <script setup lang="ts">
-import {marked} from "marked";
 import type { UIMessage } from "ai";
+import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github.css';
+const marked = new Marked(
+  markedHighlight({
+	emptyLangClass: 'hljs',
+    langPrefix: 'hljs language-',
+    highlight(code, lang, info) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    }
+  })
+);
 
 const props = defineProps<{
   messages: UIMessage[];
