c71791170e5eceaca6ca46a98adbda7bf927b1f8
stravo1@mac <stravoone@gmail.com>
2025-06-28 00:01:51 +0530

    misc(ui): minor tweaks

--- components/LoaderModal.vue
+++ components/LoaderModal.vue
@@ -4,8 +4,8 @@ import { LoaderCircle } from 'lucide-vue-next';
 
 <template>
   <div
-    class="fixed inset-0 z-[100] flex h-[100dvh] w-screen items-center justify-center bg-[rgba(255,255,255,0.5)] text-black backdrop-blur-sm dark:bg-[rgba(0,0,0,0.5)] dark:text-white"
+    class="bg-background text-foreground fixed inset-0 z-[100] flex h-[100dvh] w-screen items-center justify-center"
   >
-    <LoaderCircle class="animate-spin text-white" />
+    <LoaderCircle class="text-foreground animate-spin" />
   </div>
 </template>
