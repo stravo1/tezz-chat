0fbc39a08f836db3f905ec6e0add9fd02d1793ec
stravo1@mac <stravoone@gmail.com>
2025-10-09 12:16:59 +0530

    fix(test-build): following instructions mentioned in build logs

--- components/ui/button/Button.vue
+++ components/ui/button/Button.vue
@@ -4,7 +4,7 @@ import { Primitive, type PrimitiveProps } from 'reka-ui';
 import { cn } from '@/lib/utils';
 import { type ButtonVariants, buttonVariants } from '.';
 
-interface Props extends PrimitiveProps {
+interface Props extends /* @vue-ignore */ PrimitiveProps {
   variant?: ButtonVariants['variant'];
   size?: ButtonVariants['size'];
   class?: HTMLAttributes['class'];
